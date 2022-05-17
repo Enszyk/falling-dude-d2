2ea3de7422c0e373fe478c4efecad7f9766140c7
Norbert Sztyk <norbert.sztyk@synergity.co>
2022-05-18 00:06:26 +0200

    Update next level camera portal (bug fix)

--- FallingDude2D/Assets/Scripts/NextLevelCameraPortal.cs
+++ FallingDude2D/Assets/Scripts/NextLevelCameraPortal.cs
@@ -6,14 +6,34 @@ using UnityEngine;
 public class NextLevelCameraPortal : MonoBehaviour
 {
     [SerializeField] private Transform nextLevel;
+    [SerializeField] private Transform previousLevel;
 
     [SerializeField] private CameraController cam;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (!col.CompareTag("Player")) return;
+        if (col.transform.position.y < transform.position.y)
         {
             cam.MoveToNextLevel(nextLevel);
         }
+        else
+        {
+            cam.MoveToNextLevel(previousLevel);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (!col.CompareTag("Player")) return;
+        
+        if (col.transform.position.y < transform.position.y)
+        {
+            cam.MoveToNextLevel(previousLevel);
+        }
+        // else
+        // {
+        //     cam.MoveToNextLevel(previousLevel);
+        // }
     }
 }
