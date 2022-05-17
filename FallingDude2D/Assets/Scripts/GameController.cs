cf4a3ce125252afec2018e873b6225eb4b613732
Norbert Sztyk <norbert.sztyk@synergity.co>
2022-05-18 00:06:48 +0200

    Add game end screen and logic for handling it

--- FallingDude2D/Assets/Scripts/GameController.cs
+++ FallingDude2D/Assets/Scripts/GameController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.Serialization;
 
 public class GameController : MonoBehaviour
 {
+    
+    
     private int timeSeconds = 0;
     [SerializeField]
     private Transform lvl1;
@@ -20,14 +23,21 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private GameObject options;
     [SerializeField]
+    private GameObject resultsScreen;
+    [SerializeField]
+    private TextMeshProUGUI resultsText;
+    [SerializeField]
     private GameObject backgroundMusic;
     [SerializeField]
     private GameObject[] buttons;
-
+    
+    public static GameController instance;
     private void Awake()
     {
+        instance = this;
         menu.SetActive(false);
         options.SetActive(false);
+        resultsScreen.SetActive(false);
 
         backgroundMusic.GetComponent<AudioSource>().volume = Settings.musicVolume;
 
@@ -35,11 +45,6 @@ public class GameController : MonoBehaviour
         ChangeTimerState();
 
         StartCoroutine(MeasureTime(1.0f));
-    }
-
-    private void Update()
-    {
-
     }
 
     public void ChangeTimerState()
@@ -51,6 +56,13 @@ public class GameController : MonoBehaviour
             timer.alpha = 1;
             DisplayTime();
         }
+    }
+
+    public void EndLevel()
+    {
+        resultsText.text = $"Time: {GetGameTime()}";
+        ShowResults();
+        Time.timeScale = 0;;
     }
 
     public void ChangeHudVisibility()
@@ -67,6 +79,11 @@ public class GameController : MonoBehaviour
     public void HideMenu()
     {
         menu.SetActive(false);
+    }
+
+    public void ShowResults()
+    {
+        resultsScreen.SetActive(true);
     }
 
     public void ShowOptions()
@@ -86,22 +103,27 @@ public class GameController : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
-    private void DisplayTime()
+    private string GetGameTime()
     {
-        int hours = timeSeconds / 3600;
-        int minutes = (timeSeconds % 3600) / 60;
-        int seconds = (timeSeconds % 3600) % 60;
+        var hours = timeSeconds / 3600;
+        var minutes = (timeSeconds % 3600) / 60;
+        var seconds = (timeSeconds % 3600) % 60;
 
-        string hoursS = $"{hours}";
+        var hoursS = $"{hours}";
         if (hours < 10)
             hoursS = "0" + hoursS;
-        string minutesS = $"{minutes}";
+        var minutesS = $"{minutes}";
         if (minutes < 10)
             minutesS = "0" + minutesS;
-        string secondsS = $"{seconds}";
+        var secondsS = $"{seconds}";
         if (seconds < 10)
             secondsS = "0" + secondsS;
-        timer.text = $"{hoursS}:{minutesS}:{secondsS}";
+        return $"{hoursS}:{minutesS}:{secondsS}";
+    }
+    private void DisplayTime()
+    {
+        
+        timer.text = GetGameTime();
     }
 
     IEnumerator MeasureTime(float seconds)
@@ -114,5 +136,7 @@ public class GameController : MonoBehaviour
                DisplayTime();
         }
     }
+    
+    
 
 }
